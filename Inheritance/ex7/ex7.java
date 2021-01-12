3f19666ed0ed4e0135fe3fe5fce993e12bba3824
Max152nn <47231074+Max152nn@users.noreply.github.com>
2021-01-12 17:22:12 +0300

    Update ex7.java

--- Inheritance/ex7/ex7.java
+++ Inheritance/ex7/ex7.java
@@ -5,22 +5,19 @@ public class ex7 {
 
     }
 
-    public enum Color{
-        BLACK,RED
-                {
-                    public Color getRed(){
-                        return Color.RED;
-                    }
-                },
-        BLUE{
-            public Color getBlue(){
-                return Color.BLUE;
-            }
-        },GREEN{
-            public Color getGreen(){
-                return Color.GREEN;
-            }
-        },
-        CYAN,MAGNETA,YELLOW,WHITE;
+    public enum Color {
+        BLACK, RED, BLUE, GREEN,CYAN, MAGNETA, YELLOW, WHITE;
+
+        public static Color getRed() {
+            return Color.RED;
+        }
+
+        public static Color getBlue() {
+            return Color.BLUE;
+        }
+
+        public static Color getGreen() {
+            return Color.GREEN;
+        }
     }
 }
